df8ea9a045d4a3022b5fa37c63866286b9169e05
Gayathri3bala <98374454+Gayathri3bala@users.noreply.github.com>
2022-02-21 22:16:37 +0530

    Add files via upload

---
+++ MyJDBCConnection/src/main/java/config/DBConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class DBConfig {
+	public static Connection getConnect(Properties props) {
+		Connection connection=null;
+		try {
+			//Get url,username,password from object
+			String driver=props.getProperty("driver");
+			String url=props.getProperty("url");
+			String username=props.getProperty("username");
+			String password=props.getProperty("password");
+			//load driver
+			Class.forName(driver);
+			connection=DriverManager.getConnection(url,username,password);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
